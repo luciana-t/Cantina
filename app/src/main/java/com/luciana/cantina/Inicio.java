b37c9659db79013c6a5cd6779a4713b7681a2a1d
luciana-t <luciana.tavares@ufv.com>
2019-05-12 11:21:06 -0300

    Icones inferiors

--- app/src/main/java/com/luciana/cantina/Inicio.java
+++ app/src/main/java/com/luciana/cantina/Inicio.java
@@ -59,7 +59,7 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
-
+        openFragment1(f2);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
