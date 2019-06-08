4efba2599e905618bdef75e0e5a7aea51c0e8075
luciana-t <luciana.tavares@ufv.com>
2019-06-08 19:38:15 -0300

    Comprador criado

---
+++ app/src/main/java/com/luciana/cantina/EspecialUser.java
@@ -0,0 +1,13 @@
+package com.luciana.cantina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EspecialUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_especial_user);
+    }
+}
