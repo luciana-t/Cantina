b00575b73530582377045e1d454e52d627b939cf
luciana-t <luciana.tavares@ufv.com>
2019-06-09 11:39:16 -0300

    Cadastro de estoque atualizado - pesos

--- app/src/main/java/com/luciana/cantina/CadastroEstoque.java
+++ app/src/main/java/com/luciana/cantina/CadastroEstoque.java
@@ -1,22 +1,34 @@
 package com.luciana.cantina;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class CadastroEstoque extends AppCompatActivity {
-    TextView tx;
-    int vez = 1;
+public class CadastroEstoque extends Activity {
     EditText nome, dtVal, qtdTotal, qtdPorcao, precoTotal, precoPorcao;
+    TextView tx;
+    Button add;
+    int vez;
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_estoque);
         vez = 1;
         tx = (TextView) findViewById(R.id.tv_vez);
-        //tx.setText(""+vez);
+        tx.setText(""+vez);
+
+        spinner = (Spinner) findViewById(R.id.sp1);
+        String [] pesos = {"   Kg" , "    g", "    L", "   ml", "unidade", "caixas"};
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item,pesos);
+        spinner.setAdapter(adapter);
 
         nome = (EditText) findViewById(R.id.edt_nome);
         qtdTotal = (EditText) findViewById(R.id.edt_qtd);
@@ -25,21 +37,21 @@ public class CadastroEstoque extends AppCompatActivity {
         precoTotal = (EditText) findViewById(R.id.edt_total);
         precoPorcao = (EditText) findViewById(R.id.edt_valorporcao);
 
-        setContentView(R.layout.activity_cadastro_estoque);
     }
     public void onClickVez(View view){
-        Intent it = new Intent(getBaseContext(), CadastroEstoque.class);
-//        vez++;
-//        it.putExtra("vez", vez);
-//        nome.setText(" ");
-//        qtdPorcao.setText(" ");
-//        qtdTotal.setText("");
-//        dtVal.setText("");
-//        precoPorcao.setText("");
-//        precoTotal.setText("");
-        startActivity(it);
+        //colocar todos no bd.
+        nome.setText("");
+        qtdTotal.setText("");
+        qtdPorcao.setText("");
+        dtVal.setText("");
+        precoTotal.setText("");
+        precoPorcao.setText("");
+        vez++;
+        tx.setText(""+vez);
     }
     public void onClickVoltar(View view){
         finish();
     }
+
+
 }
